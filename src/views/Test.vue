ecdceb89fd5e8a9550aea9a2a1bdef4dfecbe274
MusicTen <1765408313@qq.com>
2020-10-10 11:39:07 +0800

    setup

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,10 +1,22 @@
 <template>
   <div>Test</div>
-  <div>content</div>
+  <div>{{ msg }}</div>
 </template>
 <script>
 export default {
   name: 'test',
+  setup(props, context) {
+    const msg = 123;
+    console.log('setup');
+    console.log('props:', props, 'context:', context);
+    return { msg };
+  },
+  // beforeCreate() {
+  //   console.log('beforeCreate');
+  // },
+  // created() {
+  //   console.log('created');
+  // },
 };
 </script>
 <style>
