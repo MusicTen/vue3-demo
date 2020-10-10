106bc5407f76abb7e102d1fe94f95dbfb8900611
MusicTen <1765408313@qq.com>
2020-10-10 15:55:02 +0800

    修改部分eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
+  extends: ["plugin:vue/vue3-essential", "@vue/airbnb"],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint"
   },
   rules: {
-    'linebreak-style': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+    "no-plusplus": "off", // ESLint认为一元操作符，是不安全的，所以禁止使用
+    "semi": "off", // 关闭语句强制分号结尾
+    "linebreak-style": "off", // 换行风格
+    "comma-dangle": ["error", "never"], // 对象字面量项尾不能有逗号
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
 };
